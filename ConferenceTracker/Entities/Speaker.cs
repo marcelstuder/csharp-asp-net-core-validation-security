dd5f5b735a0e122164228d54a7a84837301f333d
Marcel Studer <marcel.studer@outlook.at>
2020-02-12 07:38:13 +0100

    Module 2 - up to stage 4 - completed

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,18 +4,47 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker : IValidatableObject
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 2)]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 2)]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
+
+        [Required]
+        [StringLength(500, MinimumLength = 10)]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
         [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
+
         [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
+
         public bool IsStaff { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var result = new List<ValidationResult>();
+            var isTechConferenceMailAddress = EmailAddress?.Equals("TechnologyLiveConference.com", StringComparison.OrdinalIgnoreCase) ?? false;
+            if (isTechConferenceMailAddress) {
+                result.Add(new ValidationResult("Technology Live Conference staff should not use their conference email addresses."));
+            }
+
+            return result;
+        }
     }
 }
